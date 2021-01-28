d29a9a444fbb51e8081ef017d8e67b6ac66db21c
Thomas Guyader <thomas.guyader@gouv.nc>
2021-01-29 10:19:38 +1100

    feat(MainScreen): saisie simplifiée de la durée

--- coockingchrono/widgets/time_input.py
+++ coockingchrono/widgets/time_input.py
@@ -6,39 +6,23 @@ class TimeInput(MDTextField):
     """
     widget from https://github.com/kivy/kivy/pull/6090/files
     """
-
+    raw_value = ""
     mask = "##:##:##"
     capital = False
 
     def insert_text(self, substring, from_undo=False):
-        first = False
-        s = ""
-        if self.mask == "":
-            s = substring
-        else:
-            mask = self.mask
-            col, row = self.cursor
-            if col < len(mask):
-                if mask[col] == "#":
-                    numbers = "1234567890"
-                    if substring in numbers:
-                        s = substring
-                elif mask[col] == "A":
-                    letters = "QWERTYUIOPASDFGHJKLÇZXCVBNMqwertyuiopasdfghjklçzxcvbnm"
-                    if substring in letters:
-                        s = substring
-                elif mask[col] == substring:
-                    s = substring
-                else:
-                    s = mask[col]
-                    first = True
-                if col < len(mask) - 1:
-                    if mask[col + 1] != "#" and mask[col + 1] != "A":
-                        s += mask[col + 1]
-                    if first:
-                        s += substring
-
-        if self.capital:
-            s = substring.upper()
-
+        Logger.debug("insert_text: %s => %s (%s)" % (self.text, substring, len(self.text)))
+        s = self._format_time(substring)
+        self.text = ""
+        #if len(self.text) == 8:
         return super(TimeInput, self).insert_text(s, from_undo=from_undo)
+        # else:
+        #     return super(TimeInput, self).insert_text("", from_undo=from_undo)
+
+    def _format_time(self, substring):
+        txt = self.text
+        txt = txt.replace(":", "")
+        txt += substring
+        txt = str(f'{int(txt):0>6}')
+        txt = ":".join((txt[0 + i:2 + i] for i in range(0, len(txt), 2)))
+        return txt
