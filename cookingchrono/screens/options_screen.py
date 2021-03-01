31caae32ace169db28482d22c809a15dcf8b6a8d
Thomas Guyader <laboitatom@gmail.com>
2021-03-01 22:22:48 +1100

    feat(options): selection fichier de son

--- cookingchrono/screens/options_screen.py
+++ cookingchrono/screens/options_screen.py
@@ -1,4 +1,7 @@
+from kivy.clock import Clock
 from kivy.lang import Builder
+from kivymd.toast import toast
+from kivymd.uix.filemanager import MDFileManager
 from kivymd.uix.screen import MDScreen
 
 Builder.load_string("""
@@ -9,8 +12,37 @@ Builder.load_string("""
         pos_hint: {"center_x": .5, "center_y": .80}
         valign: 'middle'
         halign: 'center'
+    FloatLayout:
+
+    MDRoundFlatIconButton:
+        text: "Open manager"
+        icon: "folder"
+        pos_hint: {'center_x': .5, 'center_y': .6}
+        on_release: self.file_manager_open()
 """)
 
 
 class OptionsScreen(MDScreen):
-    pass
+    def on_enter(self):
+        Clock.schedule_once(self._init_widget)
+
+    def _init_widget(self, arg):
+        self.manager_open = False
+        self.file_manager = MDFileManager(
+            exit_manager=self.exit_manager,
+            select_path=self.select_path,
+            preview=False,
+        )
+
+    def file_manager_open(self):
+        self.file_manager.show('/')  # output manager to the screen
+        self.manager_open = True
+
+    def select_path(self, path):
+        self.exit_manager()
+        toast(path)
+
+    def exit_manager(self, *args):
+        self.manager_open = False
+        self.file_manager.close()
+
