592118464a26e65ea42de532efe12ccbc9a74394
Thomas Guyader <thomas.guyader@gouv.nc>
2021-03-02 11:08:53 +1100

    chore(screen_factory): factory fonctionnelle

--- cookingchrono/main.py
+++ cookingchrono/main.py
@@ -4,22 +4,22 @@ import os
 from kivy import Logger
 from kivy.properties import ObjectProperty
 from kivymd.app import MDApp
-from tinydb import TinyDB
-
 from screens.screen_factory import ScreenFactory
+from tinydb import TinyDB
 
 Logger.setLevel(logging.DEBUG)
 
 
 class CookingChronoApp(MDApp):
     manager = ObjectProperty(None)
+    db = ObjectProperty(None)
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
 
         dirname = os.path.dirname(os.path.abspath(__file__))
         self.db = TinyDB(os.path.join(dirname, "db", "db.json"))
-        from screens.main_screen import MainScreen
+
         ScreenFactory.create_screens()
         return
 
--- cookingchrono/screens/screen_factory.py
+++ cookingchrono/screens/screen_factory.py
@@ -12,43 +12,43 @@ class ScreenFactory:
     """ Internal registry for available screens """
 
     @classmethod
-    def register(cls, name: str) -> Callable:
-        """ Class method to register MDScreen class to the internal registry.
+    def register(cls, name: str, icon: str = "", text: str = "", default: bool = False) -> Callable:
+        """Class method to register MDScreen class to the internal registry.
         Args:
             name (str): The name of the screen.
+            default (bool): Is default screen
         Returns:
             The Screen class itself.
         """
-        Logger.debug("register %s "% name)
+        Logger.debug("register %s " % name)
 
         def inner_wrapper(wrapped_class: MDScreen) -> Callable:
             if name in cls.registry:
-                Logger.warning('Screen %s already exists. Will replace it', name)
-            cls.registry[name] = wrapped_class
+                Logger.warning("Screen %s already exists. Will replace it", name)
+            cls.registry[name] = (wrapped_class, {"default": default, "icon": icon, "text": text})
             return wrapped_class
 
         return inner_wrapper
 
-    # end register()
 
     @classmethod
-    def create_screens(cls, **kwargs) -> 'MDScreen':
-        """ Factory command to create the executor.
+    def create_screens(cls, **kwargs) -> "MDScreen":
+        """Factory command to create the executor.
         This method gets the appropriate Executor class from the registry
         and creates an instance of it, while passing in the parameters
         given in ``kwargs``.
-        Args:
-            name (str): The name of the executor to create.
         Returns:
             An instance of the executor that is created.
         """
+
         for name in cls.registry:
             screen_class = cls.registry[name]
-            screen = screen_class(**kwargs)
-            Logger.debug(screen)
-            App.get_running_app().screen_manager.add_widget(screen())
-        return
+            screen = screen_class[0](**kwargs)
+            if screen_class[1]["default"]:
+                default_screen = screen
+            #Logger.debug(App.get_running_app().menu_list)
+            App.get_running_app().manager.add_widget(screen)
 
-    # end create_executor()
-
-# end class ExecutorFactory
+        if default_screen is not None:
+            Logger.debug("set default_screen : %s " % default_screen.name)
+            App.get_running_app().manager.current = default_screen.name
