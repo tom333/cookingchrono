23c4593dbe668a8ffb6c5ff8aa9fd9094c292257
Thomas Guyader <laboitatom@gmail.com>
2021-03-26 08:42:32 +1100

    feat(android): demande d'accès au systeme de fichier sur android

--- cookingchrono/main.py
+++ cookingchrono/main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from kivy import platform
 
 from garden.screens.screen_factory import ScreenFactory
 from kivy import Logger
@@ -19,7 +20,36 @@ class CookingChronoApp(MDApp):
     db = ObjectProperty(None)
     menu_list = ObjectProperty(None)
 
+    def request_android_permissions(self):
+        """
+        Since API 23, Android requires permission to be requested at runtime.
+        This function requests permission and handles the response via a
+        callback.
+        The request will produce a popup if permissions have not already been
+        been granted, otherwise it will do nothing.
+        """
+
+        def callback(permissions, results):
+            """
+            Defines the callback to be fired when runtime permission
+            has been granted or denied. This is not strictly required,
+            but added for the sake of completeness.
+            """
+            if all([res for res in results]):
+                print("callback. All permissions granted.")
+            else:
+                print("callback. Some permissions refused.")
+
+        from android.permissions import request_permissions, Permission
+
+        request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE],
+                            callback)
+
     def build(self):
+        if platform == "android":
+            print("Android detected. Requesting permissions")
+            self.request_android_permissions()
+
         self.theme_cls.theme_style = "Dark"
 
         dirname = os.path.dirname(os.path.abspath(__file__))
@@ -28,6 +58,12 @@ class CookingChronoApp(MDApp):
         self.config.read(os.path.join(dirname, "db", "config.ini"))
         self.sound_player = SoundPlayer()
         ScreenFactory.create_screens()
+
+        if platform == 'android':
+            import android
+            os.environ["KIVY_AUDIO"] = "ffpyplayer"
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE])
         Logger.info("################ app built ################")
 
         return
