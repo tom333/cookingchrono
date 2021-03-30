a058be3a8202b25df9bffde32f8b35c8abc81df8
Thomas Guyader <laboitatom@gmail.com>
2021-03-31 08:41:16 +1100

    feat(Chrono): lancement du chronomètre en arrière plan

--- cookingchrono/screens/count_down_screen.py
+++ cookingchrono/screens/count_down_screen.py
@@ -1,12 +1,14 @@
 import datetime
 
 from garden.screens.screen_factory import ScreenFactory
+from jnius import autoclass
 from kivy import Logger
 from kivy.app import App
-from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.properties import NumericProperty
 from kivymd.uix.screen import MDScreen
+from oscpy.client import OSCClient
+from oscpy.server import OSCThreadServer
 from plyer import notification
 
 Builder.load_string(
@@ -48,15 +50,23 @@ Builder.load_string(
 
 @ScreenFactory.register("CountDownScreen")
 class CountDownScreen(MDScreen):
-    duration = NumericProperty(0)
-    count_down = None
-    running = True
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.running = True
+        self.duration = NumericProperty(0)
+        SERVICE_NAME = "{packagename}.Service{servicename}".format(packagename="com.moi.cookingchrono", servicename="Cookingchrono")
+        self.service = autoclass(SERVICE_NAME)
+        self.server = OSCThreadServer()
+        self.server.listen(address=b"localhost", port=3002, default=True)
+
+        self.server.bind(b"/count_down", self.update_timer)
+        self.client = OSCClient(b"localhost", 3000)
 
     def pause_count_down(self):
-        Logger.debug("pause_count_down %s " % self.count_down)
+        Logger.debug("pause_count_down")
+        self.client.send_message(b"/pause", [])
         if self.running:
             App.get_running_app().sound_player.stop()
-            self.count_down.cancel()
             self.running = False
             self.ids.pausebutton.icon = "play-circle-outline"
         else:
@@ -64,8 +74,6 @@ class CountDownScreen(MDScreen):
 
     def reinit_count_down(self):
         self.running = False
-        self.count_down.cancel()
-        self.count_down = None
         self.duration = 0
         self.manager.current = "MainScreen"
         self.manager.current_screen.duration = 0
@@ -73,11 +81,13 @@ class CountDownScreen(MDScreen):
     def start_timer(self):
         Logger.debug("start_timer")
         self.running = True
-        self.count_down = Clock.schedule_interval(self.update_timer, 1)
+        mActivity = autoclass("org.kivy.android.PythonActivity").mActivity
+        argument = str(self.duration)
+        self.service.start(mActivity, argument)
         self.ids.pausebutton.icon = "pause-circle-outline"
 
-    def update_timer(self, *args):
-        self.duration -= 1
+    def update_timer(self, duration):
+        self.duration = int(duration.decode("utf-8"))
         self.ids.timerlabel.text = self._convert_duration_to_mask()
         self.ids.pausebutton.icon = "pause-circle-outline"
         if self.duration % 60 == 0:
